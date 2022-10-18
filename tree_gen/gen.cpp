18a7867b28f97e0aa01e94c491b1cdee15b1d9cf
asdru22 <asdru22corp@gmail.com>
2022-10-19 00:29:07 +0200

    Update gen.cpp

--- tree_gen/gen.cpp
+++ tree_gen/gen.cpp
@@ -9,7 +9,8 @@ const string g1 = "{\"texture_size\":[32,32],\"textures\":{\"0\":\"asy:block/bil
 const string g2 = "\"},\"elements\":[{\"from\":[0,0,8],\"to\":[16,16,8],\"faces\":{\"north\":{\"uv\":[16,0,0,16],\"texture\":\"#0\"},\"east\":{\"uv\":[0,0,0,8],\"texture\":\"#0\"},\"south\":{\"uv\":[0,0,16,16],\"texture\":\"#0\"},\"west\":{\"uv\":[0,0,0,8],\"texture\":\"#0\"},\"up\":{\"uv\":[8,0,0,0],\"texture\":\"#0\"},\"down\":{\"uv\":[8,0,0,0],\"texture\":\"#0\"}}}],\"display\":{\"head\":{\"rotation\":[0,90.5,0],\"translation\":[7.75,-33,0],\"scale\":[0.8,0.8,0.8]}}}";
 
 int models(string type) {
-	int cmd;
+	int cmd=0;
+	cout << type;
 	if (type == "$en") cmd = 1;
 	else if (type == "$es") cmd = 2;
 	else if (type == "$ew") cmd = 3;
@@ -19,56 +20,55 @@ int models(string type) {
 	else if (type == "$wns") cmd = 7;
 	else if (type == "$ens") cmd = 8;
 
-	else (cmd*2+1)/2;
+	else (cmd * 2 + 1) / 2;
 	return cmd;
 }
 int main() {
-	ofstream file,redstone,gunpowder,writer;
+	ofstream file, redstone, gunpowder, writer;
 	file.open("C:/Users/aln20/Documents/GitHub/electromancy/datapack/data/asy/functions/block/billboard/handling/tree.mcfunction");
-	redstone.open("C:Users/aln20/Documents/GitHub/electromancy/resourcepack/assets/minecraft/models/item/redstone.json");
+	redstone. open("C:/Users/aln20/Documents/GitHub/electromancy/resourcepack/assets/minecraft/models/item/redstone.json");
 	gunpowder.open("C:/Users/aln20/Documents/GitHub/electromancy/resourcepack/assets/minecraft/models/item/gunpowder.json");
 
-	int x, y, cmd,counter =0,iter=0;
+	int x, y, cmd, counter = 0, iter = 0;
 	ifstream infile;
-	string str,type;
-	infile.open("in.txt");
+	string str, type;
+	infile.open("C:/Users/aln20/Documents/GitHub/electromancy/tree_gen/in.txt");
 
-	gunpowder<<"{\"parent\": \"minecraft:item/generated\",\"textures\": {\"layer0\": \"minecraft:item/gunpowder\"},\"overrides\": [";
-	redstone<<"{\"parent\": \"minecraft:item/generated\",\"textures\": {\"layer0\": \"minecraft:item/redstone\"},\"overrides\": [";
+	gunpowder << "{\"parent\": \"minecraft:item/generated\",\"textures\": {\"layer0\": \"minecraft:item/gunpowder\"},\"overrides\": [";
+	redstone << "{\"parent\": \"minecraft:item/generated\",\"textures\": {\"layer0\": \"minecraft:item/redstone\"},\"overrides\": [";
 
 	if (infile.fail()) cout << "fail";
 	while (infile >> str) {
 		//cout << str << endl;
 		if (counter == 0) {
-			if(type[0]=='$') cmd = models(str);
 			type = str;
+			if (type[0] == '$') cmd = models(str);
 		}
 		else if (counter == 1)  x = stoi(str);
-		else if (counter == 2)  {
+		else if (counter == 2) {
 			y = stoi(str);
-			file << "summon armor_stand ~ ~" << y / 2.0 << " ~"<< x / 2.0;
+			file << "summon armor_stand ~ ~" << y / 2.0 << " ~" << x / 2.0;
 			file << " {NoGravity:1b, Invulnerable : 1b, Marker : 1b, Invisible:1b, Tags : [\"asy.billboard\", \"asy.display\", \"asy.setup\"], ArmorItems : [{}, {}, {}, { id:\"minecraft:";
-			if(type[0]=='$') {
-				file<< "quartz";
-				type= "connector";
-				file <<"\",Count : 1b,tag : {CustomModelData:" << 6901000 + cmd << "}}]}\n";
+			if (type[0] == '$') {// if connector
+				type = "connector";
+				file << "quartz\",Count : 1b,tag : {CustomModelData:" << 6901000 + cmd << "}}]}\n";
 
 			}
-			else{
-				file<<"gunpowder";
-				redstone << "{\"predicate\": {\"custom_model_data\": "<<6901000 + iter<<"},\"model\": \"asy:block/billboard/display/"<<type.erase(0,1)<<"\"},\n";
-				gunpowder << "{\"predicate\": {\"custom_model_data\": "<<6901000 + iter<<"},\"model\": \"asy:block/billboard/button/"<<type<<"\"},\n";
-				file <<"\",Count : 1b,tag : {CustomModelData:" << 6901000 + iter << ",asy:{cmd:" << 6901000 + iter << ",click:\"" << type << "\"}}}]}\n";
+			else {
+				file << "gunpowder\",Count : 1b,tag : {CustomModelData:" << 6901000 + iter << ",asy:{cmd:" << 6901000 + iter << ",click:\"" << type.erase(0, 1) << "\"}}}]}\n";
 
-				writer.open("gunpowder/"+type+".json");
-				writer<<g1<<type<<g2;
+				redstone << "{\"predicate\": {\"custom_model_data\": " << 6901000 + iter << "},\"model\": \"asy:block/billboard/display/" << type << "\"},\n";
+				gunpowder << "{\"predicate\": {\"custom_model_data\": " << 6901000 + iter << "},\"model\": \"asy:block/billboard/buttons/" << type << "\"},\n";
+
+				writer.open("C:/Users/aln20/Documents/GitHub/electromancy/resourcepack/assets/asy/models/block/billboard/buttons/" + type + ".json");
+				writer << g1 << type << g2;
 				writer.close();
 
-				writer.open("redstone/"+type+".json");
-				writer<<r1<<type<<r2;
+				writer.open("C:/Users/aln20/Documents/GitHub/electromancy/resourcepack/assets/asy/models/block/billboard/display/" + type + ".json");
+				writer << r1 << type << r2;
 				writer.close();
 
-				iter+=1;
+				iter += 1;
 			}
 			cmd = 0;
 			counter = -1;
@@ -76,11 +76,13 @@ int main() {
 		counter += 1;
 	}
 	file << "\nexecute as @e[type=#asy:billboard,tag=asy.billboard,tag=asy.setup] run function asy:block/billboard/handling/setup";
-	redstone<<"]}";
-	gunpowder<<"]}";
+	redstone << "]}";
+	gunpowder << "]}";
+	redstone.close();
+	gunpowder.close();
+
+	infile.close();
 
 	infile.close();
 	return 0;
 }
-
-//quartz: connectors, redstone: display,gunpowder:button
